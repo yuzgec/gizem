0f1dbd96a34672088d410f18f02a50c0e7bab3e3
yuzgec <olcayy@gmail.com>
2025-01-13 16:30:58 +0300

    Fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,22 @@
 @extends('frontend.layout.app')
 @section('content')
+<section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7" 
+style="background-image: url(img/page-header/page-header-about-us.jpg);">
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-12 align-self-center p-static order-2 text-center">
+            <h1 class="text-9 font-weight-bold">Kayıt Ol</h1>
+            <span class="sub-title">{{ config('settings.siteTitle')}}</span>
+        </div>
+        <div class="col-md-12 align-self-center order-1">
+            <ul class="breadcrumb breadcrumb-light d-block text-center">
+                <li><a href="{{ route('home')}}">Anasayfa</a></li>
+                <li class="active">Kayıt Ol</li>
+            </ul>
+        </div>
+    </div>
+</div>
+</section>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mt-100 mb-50" >
